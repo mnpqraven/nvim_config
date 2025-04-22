3c70bcdfeecf3e8824d6f23e3298cb39da7e3529
Othi <mnpq.raven@gmail.com>
2025-04-22 14:35:49 +0700

    refactor(luasnip)

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -5,5 +5,4 @@ ls.filetype_extend("typescriptreact", { "typescript", "javascript" })
 ls.filetype_extend("typescript", { "javascript" })
 ls.filetype_extend("javascriptreact", { "javascript" })
 
-require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets/" })
--- require("luasnip.loaders.from_vscode").lazy_load({paths = "~/.config/nvim/vscode_snippets/"})
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets/lua"})
